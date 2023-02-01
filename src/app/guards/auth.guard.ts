494f93c6a6a0bf2f47ec2848d325c507223d0387
JonasBjorhei <jonas.kjeks@gmail.com>
2023-02-01 13:57:40 +0100

    docs: added some comments
    added some comments to the auth guard part

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -17,9 +17,11 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.userService.user) {
+    //Checks if there is any user in the session storage, incase there is nothing will happen
+      if (this.userService.user) {
       return true;
     }
+    //If there is no user in session storage then they will be sent back to the login page
     else {
       this.router.navigateByUrl("/login");
       return false;
