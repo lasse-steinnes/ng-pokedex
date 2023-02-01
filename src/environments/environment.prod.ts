2fa58ec783665ccdd01059f3b716a36f1ac8eb58
lasse-steinnes <54574913+lasse-steinnes@users.noreply.github.com>
2023-02-01 21:00:46 +0100

    feat: add pokemonAPI to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
     apiTrainers: "https://team-pokedex.glitch.me/trainers",
-    API_KEY: "T56xc6KzCE2b00aaZvi0MA=="
+    API_KEY: "T56xc6KzCE2b00aaZvi0MA==",
+    apiPokemon: "https://pokeapi.co/api/v2/pokemon" 
+    // limit: ?limit=151  ?limit=60&offset=60. ?limit=1008 <-- get all current pokemon
 };
--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -2,8 +2,8 @@
 export const environment = {
     production: false,
     apiTrainers: "https://team-pokedex.glitch.me/trainers",
-    API_KEY: "T56xc6KzCE2b00aaZvi0MA=="
-    
+    API_KEY: "T56xc6KzCE2b00aaZvi0MA==",
+    apiPokemon: "https://pokeapi.co/api/v2/pokemon"
 };
 
 // https://team-pokedex.glitch.me/trainers
