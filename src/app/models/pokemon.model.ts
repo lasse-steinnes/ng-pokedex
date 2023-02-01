68c0c1fa35b107e2eb6a2f35874c86c3f885b6ac
lasse-steinnes <54574913+lasse-steinnes@users.noreply.github.com>
2023-02-01 21:54:57 +0100

    feat: pokemon-catalogue-service in progress

---
+++ src/app/models/pokemon.model.ts
@@ -0,0 +1,7 @@
+export interface Pokemon {
+    name: "poke-name",
+    url: "poke-url" 
+}
+
+// extract id from last /x/ x=id
+// then get png from https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/x.png
--- src/app/services/catalogue/catalogue.service.ts
+++ src/app/services/catalogue/catalogue.service.ts
@@ -1,1 +1,28 @@
 // catalogue service for the pokemon catalogue.
+import {HttpClient} from '@angular/common/http'
+import {Injectable} from '@angular/core';
+import { environment } from 'src/environments/environments';
+import {Pokemon} from '../../models/pokemon.model'
+const { apiPokemon } =  environment;
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PokemonCatalogueService {
+    constructor(private readonly http: HttpClient) {}
+
+    public findAllPokemon(): void {
+        const getEmAll = "?limit=1008"
+        this.http.get<Pokemon []>(`${apiPokemon}${getEmAll}`) // list of pokemons
+        .subscribe({
+            next: () => { // in completion 
+
+            },
+            error: () => { // when something goes wrong
+
+            } 
+        })
+    }
+}
+
+// see for reference: https://gitlab.com/deanvons/guitar-purchaserizer/-/blob/master/src/app/models/guitar.ts
