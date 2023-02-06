fa51de2a41283c8a5d71fae1824d14845dfb65a8
Bror Nydal <47297059+BrorNydal@users.noreply.github.com>
2023-02-06 10:07:40 +0100

    feat: logout now logs you out.

--- src/app/components/navigate-button/navigate-button.component.ts
+++ src/app/components/navigate-button/navigate-button.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from "@angular/core";
 import { Router } from "@angular/router";
+import { UserService } from "src/app/services/user/user.service";
 
 @Component({
     selector: "app-navigate-button",
@@ -11,9 +12,12 @@ export class NavigateButton{
     @Input() content:string = "";
     @Input() route:string = "";
 
-    constructor(private readonly router:Router){}
+    constructor(private readonly router:Router, private readonly userService:UserService){}
 
     navigate():void{
+        if(this.route === "login")
+            this.userService.user = undefined;
+
         this.router.navigateByUrl(this.route);
     }
 }
